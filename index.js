a3d410858d7c222bf31606a090d070575af88382
RedPandaSmash <redpandasmash@gmail.com>
2025-06-19 17:34:27 -0400

    progress, ask for help

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/first-word", (req, res) => {});
